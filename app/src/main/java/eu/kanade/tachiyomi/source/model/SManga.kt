cd741546c5380ddb7481e9cf2f26bc216db5f2b2
arkon <arkon@users.noreply.github.com>
2022-04-01 19:41:51 -0400

    Add required changes for extensions-lib 1.3 (#1184)

--- app/src/main/java/eu/kanade/tachiyomi/source/model/SManga.kt
+++ app/src/main/java/eu/kanade/tachiyomi/source/model/SManga.kt
@@ -62,6 +62,10 @@ interface SManga : Serializable {
         const val ONGOING = 1
         const val COMPLETED = 2
         const val LICENSED = 3
+        const val PUBLISHING_FINISHED = 4
+        const val CANCELLED = 5
+        const val ON_HIATUS = 6
+
         const val RECOMMENDS = 69 // nice
 
         fun create(): SManga {
