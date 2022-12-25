328674feb805f59bef90507f25bfb42f36a93171
az4521 <az4521@users.noreply.github.com>
2022-12-25 00:53:20 -0500

    somehow this fixed everything

--- app/src/main/java/eu/kanade/tachiyomi/ui/source/browse/BrowseSourcePresenter.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/source/browse/BrowseSourcePresenter.kt
@@ -33,8 +33,7 @@ import eu.kanade.tachiyomi.ui.source.filter.TriStateSectionItem
 import eu.kanade.tachiyomi.util.lang.launchIO
 import eu.kanade.tachiyomi.util.lang.launchUI
 import eu.kanade.tachiyomi.util.removeCovers
-import exh.EH_SOURCE_ID
-import exh.EXH_SOURCE_ID
+import exh.isEhBasedSource
 import exh.savedsearches.EXHSavedSearch
 import exh.savedsearches.JsonSavedSearch
 import java.lang.RuntimeException
@@ -289,7 +288,7 @@ open class BrowseSourcePresenter(
     }
 
     open fun createPager(query: String, filters: FilterList): Pager {
-        return if (source.id in listOf(EXH_SOURCE_ID, EH_SOURCE_ID)) { ExhPager(source, query, filters) } else { SourcePager(source, query, filters) }
+        return if (source.isEhBasedSource()) { ExhPager(source, query, filters) } else { SourcePager(source, query, filters) }
     }
 
     private fun FilterList.toItems(): List<IFlexible<*>> {
--- app/src/main/java/eu/kanade/tachiyomi/ui/source/latest/LatestUpdatesPresenter.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/source/latest/LatestUpdatesPresenter.kt
@@ -3,8 +3,7 @@ package eu.kanade.tachiyomi.ui.source.latest
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.ui.source.browse.BrowseSourcePresenter
 import eu.kanade.tachiyomi.ui.source.browse.Pager
-import exh.EH_SOURCE_ID
-import exh.EXH_SOURCE_ID
+import exh.isEhBasedSource
 
 /**
  * Presenter of [LatestUpdatesController]. Inherit BrowseCataloguePresenter.
@@ -12,6 +11,6 @@ import exh.EXH_SOURCE_ID
 class LatestUpdatesPresenter(sourceId: Long) : BrowseSourcePresenter(sourceId) {
 
     override fun createPager(query: String, filters: FilterList): Pager {
-        return if (source.id in listOf(EXH_SOURCE_ID, EH_SOURCE_ID)) { ExhLatestUpdatesPager(source) } else { LatestUpdatesPager(source) }
+        return if (source.isEhBasedSource()) { ExhLatestUpdatesPager(source) } else { LatestUpdatesPager(source) }
     }
 }
