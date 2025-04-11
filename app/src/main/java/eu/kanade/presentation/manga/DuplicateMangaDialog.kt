5f68db051613675e5cba78f3acdcc5da23a55239
NarwhalHorns <onefailedgamer@gamil.com>
2025-04-11 17:19:19 +0100

    spotless

--- app/src/main/java/eu/kanade/presentation/manga/DuplicateMangaDialog.kt
+++ app/src/main/java/eu/kanade/presentation/manga/DuplicateMangaDialog.kt
@@ -1,87 +1,39 @@
 package eu.kanade.presentation.manga
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowColumn
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Brush
-import androidx.compose.material.icons.filled.PersonOutline
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material.icons.outlined.Add
-import androidx.compose.material.icons.outlined.AttachMoney
-import androidx.compose.material.icons.outlined.Block
-import androidx.compose.material.icons.outlined.Close
-import androidx.compose.material.icons.outlined.Done
-import androidx.compose.material.icons.outlined.DoneAll
-import androidx.compose.material.icons.outlined.Pause
-import androidx.compose.material.icons.outlined.Schedule
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.TextMeasurer
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.rememberTextMeasurer
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.Constraints
-import androidx.compose.ui.unit.Density
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.util.fastMaxOfOrNull
-import eu.kanade.domain.ui.UiPreferences
 import eu.kanade.presentation.components.AdaptiveSheet
 import eu.kanade.presentation.components.TabbedDialogPaddings
 import eu.kanade.presentation.duplicates.components.DuplicateMangaListItem
 import eu.kanade.presentation.duplicates.components.getMaximumMangaCardHeight
-import eu.kanade.presentation.manga.components.MangaCover
-import eu.kanade.presentation.manga.components.RatioSwitchToPanorama
 import eu.kanade.presentation.more.settings.LocalPreferenceMinHeight
 import eu.kanade.presentation.more.settings.widget.TextPreferenceWidget
-import eu.kanade.tachiyomi.source.Source
-import eu.kanade.tachiyomi.source.model.SManga
 import tachiyomi.domain.manga.model.Manga
 import tachiyomi.domain.manga.model.MangaWithChapterCount
-import tachiyomi.domain.source.model.StubSource
 import tachiyomi.domain.source.service.SourceManager
 import tachiyomi.i18n.MR
-import tachiyomi.i18n.kmk.KMR
-import tachiyomi.presentation.core.components.Badge
-import tachiyomi.presentation.core.components.BadgeGroup
 import tachiyomi.presentation.core.components.material.padding
-import tachiyomi.presentation.core.i18n.pluralStringResource
 import tachiyomi.presentation.core.i18n.stringResource
-import tachiyomi.presentation.core.util.collectAsState
-import tachiyomi.presentation.core.util.secondaryItemAlpha
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 
@@ -93,14 +45,6 @@ fun DuplicateMangaDialog(
     onOpenManga: (manga: Manga) -> Unit,
     onMigrate: (manga: Manga) -> Unit,
     modifier: Modifier = Modifier,
-    // KMK -->
-    targetManga: Manga,
-    bulkFavoriteManga: Manga? = null,
-    onAllowAllDuplicate: () -> Unit = {},
-    onSkipAllDuplicate: () -> Unit = {},
-    onSkipDuplicate: () -> Unit = {},
-    stopRunning: () -> Unit = {},
-    // KMK <--
 ) {
     val sourceManager = remember { Injekt.get<SourceManager>() }
     val minHeight = LocalPreferenceMinHeight.current
@@ -109,12 +53,7 @@ fun DuplicateMangaDialog(
 
     AdaptiveSheet(
         modifier = modifier,
-        onDismissRequest = {
-            // KMK -->
-            stopRunning()
-            // KMK <--
-            onDismissRequest()
-        },
+        onDismissRequest = onDismissRequest,
     ) {
         Column(
             modifier = Modifier
@@ -131,14 +70,6 @@ fun DuplicateMangaDialog(
                     .padding(top = MaterialTheme.padding.small),
             )
 
-            // KMK -->
-            Text(
-                text = targetManga.title,
-                style = MaterialTheme.typography.titleMedium,
-                modifier = Modifier.then(horizontalPaddingModifier),
-            )
-            // KMK <--
-
             Text(
                 text = stringResource(MR.strings.possible_duplicates_summary),
                 style = MaterialTheme.typography.bodyMedium,
@@ -146,19 +77,14 @@ fun DuplicateMangaDialog(
             )
 
             LazyRow(
-                horizontalArrangement = /* KMK --> */ Arrangement.SpaceAround, /* KMK <-- */
-                modifier = Modifier.height(getMaximumMangaCardHeight(duplicates))
-                    // KMK -->
-                    .fillMaxWidth(),
-                // KMK <--
+                horizontalArrangement = Arrangement.spacedBy(MaterialTheme.padding.small),
+                modifier = Modifier.height(getMaximumMangaCardHeight(duplicates)),
                 contentPadding = horizontalPadding,
             ) {
-                // KMK -->
-                itemsIndexed(
-                    // KMK <--
+                items(
                     items = duplicates,
-                    key = { _, it -> it.manga.id },
-                ) { index, it ->
+                    key = { it.manga.id },
+                ) {
                     DuplicateMangaListItem(
                         duplicate = it,
                         getSource = { sourceManager.getOrStub(it.manga.source) },
@@ -166,124 +92,37 @@ fun DuplicateMangaDialog(
                         onDismissRequest = onDismissRequest,
                         onLongClick = { onOpenManga(it.manga) },
                     )
-
-                    // KMK -->
-                    if (index != duplicates.lastIndex) {
-                        Spacer(modifier = Modifier.width(MaterialTheme.padding.small))
-                    }
-                    // KMK <--
                 }
             }
 
-            // KMK -->
-            if (bulkFavoriteManga != null) {
-                Column(
-                    modifier = horizontalPaddingModifier
-                        .padding(bottom = MaterialTheme.padding.medium),
-                ) {
-                    HorizontalDivider()
+            Column(modifier = horizontalPaddingModifier) {
+                HorizontalDivider()
 
-                    FlowRow(
-                        Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceAround,
-                    ) {
-                        FlowColumn {
-                            TextButton(
-                                onClick = {
-                                    onDismissRequest()
-                                    onConfirm()
-                                },
-                                Modifier.align(Alignment.CenterHorizontally),
-                            ) {
-                                Text(text = stringResource(MR.strings.action_add_anyway))
-                            }
+                TextPreferenceWidget(
+                    title = stringResource(MR.strings.action_add_anyway),
+                    icon = Icons.Outlined.Add,
+                    onPreferenceClick = {
+                        onDismissRequest()
+                        onConfirm()
+                    },
+                    modifier = Modifier.clip(CircleShape),
+                )
+            }
 
-                            TextButton(
-                                onClick = {
-                                    onDismissRequest()
-                                    onAllowAllDuplicate()
-                                },
-                                Modifier.align(Alignment.CenterHorizontally),
-                            ) {
-                                Text(text = stringResource(KMR.strings.action_allow_all_duplicate_manga))
-                            }
-                        }
-
-                        FlowColumn {
-                            TextButton(
-                                onClick = {
-                                    onDismissRequest()
-                                    onSkipDuplicate()
-                                },
-                                Modifier.align(Alignment.CenterHorizontally),
-                            ) {
-                                Text(text = stringResource(KMR.strings.action_skip_duplicate_manga))
-                            }
-
-                            TextButton(
-                                onClick = {
-                                    onDismissRequest()
-                                    onSkipAllDuplicate()
-                                },
-                                Modifier.align(Alignment.CenterHorizontally),
-                            ) {
-                                Text(text = stringResource(KMR.strings.action_skip_all_duplicate_manga))
-                            }
-                        }
-
-                        FlowColumn {
-                            TextButton(
-                                onClick = {
-                                    onOpenManga(bulkFavoriteManga)
-                                },
-                                Modifier.align(Alignment.CenterHorizontally),
-                            ) {
-                                Text(text = stringResource(MR.strings.action_show_manga))
-                            }
-
-                            TextButton(
-                                onClick = {
-                                    stopRunning()
-                                    onDismissRequest()
-                                },
-                                Modifier.align(Alignment.CenterHorizontally),
-                            ) {
-                                Text(text = stringResource(MR.strings.action_cancel))
-                            }
-                        }
-                    }
-                }
-            } else {
-                // KMK <--
-                Column(modifier = horizontalPaddingModifier) {
-                    HorizontalDivider()
-
-                    TextPreferenceWidget(
-                        title = stringResource(MR.strings.action_add_anyway),
-                        icon = Icons.Outlined.Add,
-                        onPreferenceClick = {
-                            onDismissRequest()
-                            onConfirm()
-                        },
-                        modifier = Modifier.clip(CircleShape),
-                    )
-                }
-
-                OutlinedButton(
-                    onClick = onDismissRequest,
-                    modifier = Modifier
-                        .then(horizontalPaddingModifier)
-                        .padding(bottom = MaterialTheme.padding.medium)
-                        .heightIn(min = minHeight)
-                        .fillMaxWidth(),
-                ) {
-                    Text(
-                        modifier = Modifier.padding(vertical = MaterialTheme.padding.extraSmall),
-                        text = stringResource(MR.strings.action_cancel),
-                        color = MaterialTheme.colorScheme.primary,
-                        style = MaterialTheme.typography.bodyLarge,
-                    )
-                }
+            OutlinedButton(
+                onClick = onDismissRequest,
+                modifier = Modifier
+                    .then(horizontalPaddingModifier)
+                    .padding(bottom = MaterialTheme.padding.medium)
+                    .heightIn(min = minHeight)
+                    .fillMaxWidth(),
+            ) {
+                Text(
+                    modifier = Modifier.padding(vertical = MaterialTheme.padding.extraSmall),
+                    text = stringResource(MR.strings.action_cancel),
+                    color = MaterialTheme.colorScheme.primary,
+                    style = MaterialTheme.typography.bodyLarge,
+                )
             }
         }
     }
