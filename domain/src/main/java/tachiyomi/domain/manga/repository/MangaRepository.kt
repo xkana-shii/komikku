0d7804ff31be4b6f08d3a230fedcac3d294de2ba
NarwhalHorns <onefailedgamer@gamil.com>
2025-04-16 11:09:53 +0100

    refactor hidden duplicate interactions into own place now it exists :)

--- domain/src/main/java/tachiyomi/domain/manga/repository/MangaRepository.kt
+++ domain/src/main/java/tachiyomi/domain/manga/repository/MangaRepository.kt
@@ -28,8 +28,6 @@ interface MangaRepository {
 
     suspend fun getDuplicateLibraryManga(id: Long, title: String): List<MangaWithChapterCount>
 
-    suspend fun addHiddenDuplicate(id1: Long, id2: Long)
-
     suspend fun getHiddenDuplicates(manga: Manga): List<MangaWithChapterCount>
 
     suspend fun getUpcomingManga(statuses: Set<Long>): Flow<List<Manga>>
@@ -54,6 +52,4 @@ interface MangaRepository {
     suspend fun getReadMangaNotInLibraryView(): List<LibraryManga>
     // SY <--
     
-    suspend fun getHiddenDuplicates(manga: Manga): List<MangaWithChapterCount>
-    suspend fun removeHiddenDuplicates(id1: Long, id2: Long)
 }
