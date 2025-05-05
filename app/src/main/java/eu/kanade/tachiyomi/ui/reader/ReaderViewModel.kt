cc745a351a47e36c3edcf0f016494f7f40cfaa4b
kana-shii <deiimos.mars@gmail.com>
2025-05-05 19:48:47 -0300

    Revert "Update ReaderViewModel.kt"
    This reverts commit d98c0d147ed22f0b5c5f41a07b7549420a4f3b41.

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderViewModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderViewModel.kt
@@ -802,10 +802,8 @@ class ReaderViewModel @JvmOverloads constructor(
         val chapterId = readerChapter.chapter.id!!
         val endTime = Date()
         val sessionReadDuration = chapterReadStartTime?.let { endTime.time - it } ?: 0
-        val lastPageRead = readerChapter.chapter.last_page_read.toLong()
 
-        upsertHistory.await(HistoryUpdate(chapterId, endTime, sessionReadDuration, lastPageRead))
-
+        upsertHistory.await(HistoryUpdate(chapterId, endTime, sessionReadDuration))
         chapterReadStartTime = null
     }
 
