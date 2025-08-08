26f5b04ce2ed898d234d2893cc1f70a6d1705870
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-08-08 17:00:55 -0300

    Revert "test"
    This reverts commit 6927b6917d92b56e258708dae9ddb5d533028728.

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/ReaderActivity.kt
@@ -87,6 +87,7 @@ import eu.kanade.tachiyomi.data.connections.discord.ReaderData
 import eu.kanade.tachiyomi.data.notification.NotificationReceiver
 import eu.kanade.tachiyomi.data.notification.Notifications
 import eu.kanade.tachiyomi.databinding.ReaderActivityBinding
+import eu.kanade.tachiyomi.source.isNsfw
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.online.HttpSource
 import eu.kanade.tachiyomi.ui.base.activity.BaseActivity
@@ -1579,7 +1580,7 @@ class ReaderActivity : BaseActivity() {
                     DiscordRPCService.setReaderActivity(
                         context = this@ReaderActivity,
                         ReaderData(
-                            incognitoMode = viewModel.incognitoMode,
+                            incognitoMode = viewModel.currentSource.value?.isNsfw() == true || viewModel.incognitoMode,
                             mangaId = manga.id,
                             mangaTitle = manga.ogTitle,
                             thumbnailUrl = manga.thumbnailUrl ?: "",
@@ -1592,8 +1593,9 @@ class ReaderActivity : BaseActivity() {
                         ),
                     )
                 } else {
-                    val lastUsedScreen = DiscordRPCService.lastUsedScreen
-                    DiscordRPCService.setScreen(this@ReaderActivity, lastUsedScreen)
+                    with(DiscordRPCService) {
+                        setScreen(this@ReaderActivity)
+                    }
                 }
             } catch (e: Exception) {
                 logcat(LogPriority.ERROR) { "Error updating Discord RPC: ${e.message}" }
