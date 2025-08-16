a4496160203048c373bec093a7c52ba9d99e699c
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-08-15 22:15:01 -0300

    Revert "Update MangaBottomActionMenu.kt"
    This reverts commit 1e4d891e52d5d111a5d8c2b463124224681192df.

--- app/src/main/java/eu/kanade/presentation/manga/components/MangaBottomActionMenu.kt
+++ app/src/main/java/eu/kanade/presentation/manga/components/MangaBottomActionMenu.kt
@@ -99,11 +99,15 @@ fun MangaBottomActionMenu(
             color = MaterialTheme.colorScheme.surfaceContainerHigh,
         ) {
             val haptic = LocalHapticFeedback.current
-            val confirm = remember { mutableStateListOf(false, false, false, false, false, false, false, false) }
+            val confirm =
+                remember {
+                    mutableStateListOf(false, false, false, false, false, false, false, false, false, false, false)
+                }
+            val confirmRange = 0..<11
             var resetJob: Job? = remember { null }
             val onLongClickItem: (Int) -> Unit = { toConfirmIndex ->
                 haptic.performHapticFeedback(HapticFeedbackType.LongPress)
-                (0..<8).forEach { i -> confirm[i] = i == toConfirmIndex }
+                (confirmRange).forEach { i -> confirm[i] = i == toConfirmIndex }
                 resetJob?.cancel()
                 resetJob = scope.launch {
                     delay(1.seconds)
@@ -218,7 +222,7 @@ internal fun RowScope.Button(
     content: (@Composable () -> Unit)? = null,
 ) {
     val animatedWeight by animateFloatAsState(
-        targetValue = if (toConfirm) 2f else 1f,
+        targetValue = if (toConfirm) 3f else 1f,
         label = "weight",
     )
     // KMK -->
