5667800c3b0924cef1d7edec7ecf494af0206ae0
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-08-16 19:50:38 -0300

    Update BulkFavoriteScreenModel.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/browse/BulkFavoriteScreenModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/browse/BulkFavoriteScreenModel.kt
@@ -263,7 +263,7 @@ class BulkFavoriteScreenModel(
             if (new.favorite) {
                 withIOContext {
                     val networkManga = source.getMangaDetails(new.toSManga())
-                    updateManga.awaitUpdateFromSource(manga, networkManga, manualFetch = false, coverCache)
+                    updateManga.awaitUpdateFromSource(manga, networkManga, false, coverCache)
                     val chapters = source.getChapterList(new.toSManga())
                     if (chapters.isEmpty()) return@withIOContext
                     syncChaptersWithSource.await(chapters, new, source, false)
@@ -356,7 +356,7 @@ class BulkFavoriteScreenModel(
             if (new.favorite) {
                 withIOContext {
                     val networkManga = source.getMangaDetails(new.toSManga())
-                    updateManga.awaitUpdateFromSource(manga, networkManga, manualFetch = false, coverCache)
+                    updateManga.awaitUpdateFromSource(manga, networkManga, false, coverCache)
                     val chapters = source.getChapterList(new.toSManga())
                     if (chapters.isEmpty()) return@withIOContext
                     syncChaptersWithSource.await(chapters, new, source, false)
