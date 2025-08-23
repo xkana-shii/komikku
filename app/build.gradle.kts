fad358725feeffaee80ab0d15ee6a371129b4679
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-08-23 15:53:19 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "app.komikku.kns"
 
         versionCode = 75
-        versionName = "1.16.1"
+        versionName = "1.16.2"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
