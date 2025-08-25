286dc44491a63eb40af92f3b7ef57cc2325ca866
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-08-25 13:31:24 -0300

    Revert "refactor Discord settings menu"
    This reverts commit c212f86bf97ffa094336f055968670138e640c59.

--- app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsConnectionScreen.kt
+++ app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsConnectionScreen.kt
@@ -92,13 +92,11 @@ object SettingsConnectionScreen : SearchableSettings {
                         },
                         openSettings = { navigator.push(SettingsDiscordScreen) },
                     ),
-                    Preference.PreferenceItem.TextPreference(
-                        title = stringResource(KMR.strings.pref_discord_configuration),
-                        enabled = connectionsManager.discord.isLogged,
-                        onClick = { navigator.push(SettingsDiscordScreen) },
-                    ),
                     Preference.PreferenceItem.InfoPreference(
                         stringResource(KMR.strings.connections_discord_info, stringResource(MR.strings.app_name)),
+                    ),
+                    Preference.PreferenceItem.InfoPreference(
+                        stringResource(KMR.strings.connections_info, stringResource(MR.strings.app_name)),
                     ),
                 ),
             ),
