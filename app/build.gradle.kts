17e8d289ebf8b340fbd577a66d4fca22063a3f6f
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-09-04 23:35:10 -0300

    Bump version name to 1.16.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "app.komikku.kns"
 
         versionCode = 75
-        versionName = "1.16.5"
+        versionName = "1.16.6"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
