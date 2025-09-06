27c10f955040fd4dddec908f0488502f19151a22
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-09-06 15:23:23 -0300

    Update LibraryScreenModel.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryScreenModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/library/LibraryScreenModel.kt
@@ -1250,7 +1250,7 @@ class LibraryScreenModel(
                                     (manga.author?.contains(query, true) != true) &&
                                     (manga.artist?.contains(query, true) != true) &&
                                     (manga.description?.contains(query, true) != true) &&
-                                    (source?.name?.contains(query, true) != true || (source?.lang != null && "${source?.name} (${source?.lang})".contains(query, true))) &&
+                                    (!(source?.name?.contains(query, true) == true && (source?.lang != null && "${source?.name} (${source?.lang})".contains(query, true)))) &&
                                     (sourceIdString != null && sourceIdString != query) &&
                                     (
                                         loggedInTrackServices.isEmpty() ||
