7665b92ee44f7b0dba21a756d859b83f3ddef794
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-09-10 09:51:11 -0300

    Merge branch 'pr/1128'
    Update BackupNotifier.kt

--- app/src/main/java/eu/kanade/tachiyomi/data/backup/restore/restorers/MangaRestorer.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/backup/restore/restorers/MangaRestorer.kt
@@ -183,6 +183,7 @@ class MangaRestorer(
         )
     }
 
+    // MIHON PATCH: restoreChapters refactored
     private suspend fun restoreChapters(manga: Manga, backupChapters: List<BackupChapter>) {
         val dbChaptersByUrl = getChaptersByMangaId.await(manga.id)
             .associateBy { it.url }
@@ -200,8 +201,48 @@ class MangaRestorer(
             }
             .partition { it.id > 0 }
 
-        insertNewChapters(newChapters)
-        updateExistingChapters(existingChapters)
+        handler.await(inTransaction = true) {
+            if (newChapters.isNotEmpty()) {
+                newChapters.forEach { chapter ->
+                    chaptersQueries.insert(
+                        chapter.mangaId,
+                        chapter.url,
+                        chapter.name,
+                        chapter.scanlator,
+                        chapter.read,
+                        chapter.bookmark,
+                        chapter.lastPageRead,
+                        chapter.chapterNumber,
+                        chapter.sourceOrder,
+                        chapter.dateFetch,
+                        chapter.dateUpload,
+                        chapter.version,
+                    )
+                }
+            }
+            if (existingChapters.isNotEmpty()) {
+                existingChapters.forEach { chapter ->
+                    chaptersQueries.update(
+                        mangaId = null,
+                        url = null,
+                        name = null,
+                        scanlator = null,
+                        read = chapter.read,
+                        bookmark = chapter.bookmark,
+                        lastPageRead = chapter.lastPageRead,
+                        chapterNumber = null,
+                        dateFetch = null,
+                        // KMK -->
+                        sourceOrder = chapter.sourceOrder,
+                        dateUpload = chapter.dateUpload,
+                        // KMK <--
+                        chapterId = chapter.id,
+                        version = chapter.version,
+                        isSyncing = 1,
+                    )
+                }
+            }
+        }
     }
 
     private fun updateChapterBasedOnSyncState(chapter: Chapter, dbChapter: Chapter): Chapter {
@@ -238,6 +279,7 @@ class MangaRestorer(
         }
     }
 
+    // MIHON PATCH: forComparison stays the same, but moved for clarity
     private fun Chapter.forComparison() =
         this.copy(
             id = 0L,
@@ -250,52 +292,6 @@ class MangaRestorer(
             lastModifiedAt = 0L,
             version = 0L,
         )
-
-    private suspend fun insertNewChapters(chapters: List<Chapter>) {
-        handler.await(true) {
-            chapters.forEach { chapter ->
-                chaptersQueries.insert(
-                    chapter.mangaId,
-                    chapter.url,
-                    chapter.name,
-                    chapter.scanlator,
-                    chapter.read,
-                    chapter.bookmark,
-                    chapter.lastPageRead,
-                    chapter.chapterNumber,
-                    chapter.sourceOrder,
-                    chapter.dateFetch,
-                    chapter.dateUpload,
-                    chapter.version,
-                )
-            }
-        }
-    }
-
-    private suspend fun updateExistingChapters(chapters: List<Chapter>) {
-        handler.await(true) {
-            chapters.forEach { chapter ->
-                chaptersQueries.update(
-                    mangaId = null,
-                    url = null,
-                    name = null,
-                    scanlator = null,
-                    read = chapter.read,
-                    bookmark = chapter.bookmark,
-                    lastPageRead = chapter.lastPageRead,
-                    chapterNumber = null,
-                    dateFetch = null,
-                    // KMK -->
-                    sourceOrder = chapter.sourceOrder,
-                    dateUpload = chapter.dateUpload,
-                    // KMK <--
-                    chapterId = chapter.id,
-                    version = chapter.version,
-                    isSyncing = 1,
-                )
-            }
-        }
-    }
 
     /**
      * Inserts manga and returns id
