ce2b60ccd0bb0c4f642135c20565ca75c3c41661
kana-shii <79055104+kana-shii@users.noreply.github.com>
2025-09-25 21:21:27 -0300

    Update DownloadCache.kt

--- app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadCache.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/download/DownloadCache.kt
@@ -468,7 +468,7 @@ class DownloadCache(
                                 .mapNotNull {
                                     when {
                                         // Ignore incomplete downloads
-                                        // it.name?.endsWith(Downloader.TMP_DIR_SUFFIX) == true -> null
+                                        it.name?.endsWith(Downloader.TMP_DIR_SUFFIX) == true -> null
                                         // Folder of images
                                         it.isDirectory -> it.name
                                         // CBZ files
