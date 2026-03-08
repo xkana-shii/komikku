16b7ede325f9b24e2693cba6e34b1d331f1fa5b3
kana-shii <79055104+kana-shii@users.noreply.github.com>
2026-03-08 20:02:11 -0300

    komikku changes 1

--- app/src/main/java/eu/kanade/tachiyomi/data/track/mangabaka/MangaBakaInterceptor.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/mangabaka/MangaBakaInterceptor.kt
@@ -29,7 +29,7 @@ class MangaBakaInterceptor(private val mangaBaka: MangaBaka) : Interceptor {
         }
 
         return originalRequest.newBuilder()
-            .header("User-Agent", "Mihon/v${BuildConfig.VERSION_NAME} (Android) (https://github.com/mihonapp/mihon)")
+            .header("User-Agent", "Komikku/v${BuildConfig.VERSION_NAME} (Android) (https://github.com/xkana-shii/komikku)")
             .addHeader("Authorization", "Bearer ${currentAuth.accessToken}")
             .build()
             .let(chain::proceed)
