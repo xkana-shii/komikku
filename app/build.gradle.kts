317872db6389a8a25d6902fa133538071139f1ec
kana-shii <deiimos.mars@gmail.com>
2026-09-22 23:05:05 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -278,6 +278,7 @@ dependencies {
     // UI libraries
     implementation(libs.material)
     implementation(libs.flexible.adapter.core)
+    implementation(libs.flexibleAdapter)
     implementation(libs.photoview)
     implementation(libs.directionalviewpager) {
         exclude(group = "androidx.viewpager", module = "viewpager")
